b7fee387e6d44ea67659fe5e5e43b09668aa6386
Mike Liu <mikeliu639@gemail.com>
2013-05-17 13:36:09 -0600

    added config in application.rb to fix the heroku error on less-rails

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module Prototypr
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # To fix the error on heroku with less-rails
     config.assets.initialize_on_precompile = false
   end
 end
