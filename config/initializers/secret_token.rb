9f7250c1a9e95a58ac808e13ae68403f5ce10996
Mike Liu <mikeliu639@gemail.com>
2013-05-17 09:11:57 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prototypr::Application.config.secret_token = '059b5427f1e0edbf4045bed4cef663361b0d2921ee539474b10f9f860c9618bc0f8285f22f223180b8f3bc7748b83b3e989467d589e9d8955447d3268039ff60'
