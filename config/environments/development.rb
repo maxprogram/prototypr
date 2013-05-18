98b465b61421b0b5a241eab9edd51ec38d92cf77
Mike Liu <mikeliu639@gemail.com>
2013-05-18 12:48:44 -0600

    paperclip is working locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Prototypr::Application.configure do
   # Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  #Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,18 @@ Prototypr::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   #devise
-  config.action_mailer.default_url_options = { :host => 'prototypr.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'prototyprdev.herokuapp.com' }
+
+  # Amazon S3 settings for Paperclip uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_permissions => :private,
+    :path => "photos/:id/:filename",
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
